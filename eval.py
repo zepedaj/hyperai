7a05ee8cab3561acc1e47b184769fd5729b1295c
Joaquin <jazs.pro@gmail.com>
2018-05-05 17:00:25 -0700

    m

---
+++ eval.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn import metrics
+
+def rp(scores, gt):
+    scores=scores.reshape(-1);
+    gt=gt.reshape(-1);
+    print('R/P:',metrics.average_precision_score(gt, scores))
+
+if __name__=='__main__':
+    scores=np.random.rand(10,10)
+    gt=np.random.randint(2,size=(10,10))
+    rp(scores, gt)
